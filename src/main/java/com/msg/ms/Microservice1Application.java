bc9b8cc6360633bad5179ca14bf6401ef8ff8625
mahmoudsgaballa <117027907+mahmoudsgaballa@users.noreply.github.com>
2022-11-04 20:48:00 +0200

    Initial commit

---
+++ src/main/java/com/msg/ms/Microservice1Application.java
@@ -0,0 +1,26 @@
+package com.msg.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Microservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice1Application.class, args);
+	}	
+	
+	@RestController
+	public class Ms1Controller{
+		
+//		@GetMapping("api/test/message")
+		@GetMapping("")
+		public String getMessage() {
+			return "This is microservice1";
+		}
+		
+	}
+
+}
